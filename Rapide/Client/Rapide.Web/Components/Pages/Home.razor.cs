2f009eb4e3cda1d07c4f7ee6e7d43b02411b14cc
Marxis Cabero <marxis.cabero@mims.com>
2025-10-15 20:48:22 +0800

    birthday celebrant list in dashboard page.

--- Rapide/Client/Rapide.Web/Components/Pages/Home.razor.cs
+++ Rapide/Client/Rapide.Web/Components/Pages/Home.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Rapide.Contracts.Services;
+using Rapide.DTO;
+using Rapide.Web.Components.Pages.Customers;
 using Rapide.Web.Components.Utilities;
 using Rapide.Web.Helpers;
 using System.Threading.Tasks;
@@ -15,16 +17,56 @@ namespace Rapide.Web.Components.Pages
         #region Dependency Injection
         [CascadingParameter]
         protected Task<AuthenticationState> AuthState { get; set; }
+
+        [Inject]
+        private IUserService UserServices { get; set; }
+        [Inject]
+        private ICustomerService CustomerServices { get; set; }
         #endregion
 
         #region Private Properties
         private bool isTechnician = false;
+
+        private List<UserDTO> userList = new();
+        private List<CustomerDTO> customerList = new();
+
+        private List<UserDTO> EmployeeBirthdayMonthList = new();
+        private List<CustomerDTO> CustomerBirthdayMonthList = new();
         #endregion
 
         protected override async Task OnInitializedAsync()
         {
             isTechnician = TokenHelper.IsRoleEqual(await AuthState, Constants.UserRoles.SeniorTechnician)
                 || TokenHelper.IsRoleEqual(await AuthState, Constants.UserRoles.JuniorTechnician);
+
+            userList = await UserServices.GetAllUserRoleAsync();
+            customerList = await CustomerServices.GetAllAsync();
+
+            var employeeBirthdayMonth = userList
+                .Where(x => x.Birthday.HasValue && x.Birthday.Value.Month == DateTime.Now.Month && x.Birthday.Value.Day >= DateTime.Now.Day)
+                .OrderBy(x => x.Birthday.Value.Day)
+                .ToList();
+
+            var customerBirhtdayMonth = customerList
+                .Where(x => x.Birthday.HasValue && x.Birthday.Value.Month == DateTime.Now.Month && x.Birthday.Value.Day >= DateTime.Now.Day)
+                .OrderBy(x => x.Birthday.Value.Day)
+                .ToList();
+
+            foreach (var ebm in employeeBirthdayMonth)
+            {
+                if (!EmployeeBirthdayMonthList.Any(x => x.Id == ebm.Id))
+                {
+                    EmployeeBirthdayMonthList.Add(ebm);
+                }
+            }
+
+            foreach (var cbm in customerBirhtdayMonth)
+            {
+                if (!CustomerBirthdayMonthList.Any(x => x.Id == cbm.Id))
+                {
+                    CustomerBirthdayMonthList.Add(cbm);
+                }
+            }
 
             await base.OnInitializedAsync();
         }
